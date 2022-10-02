f29468d3ae270ccc61aa126bad627374cfd80cf9
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-10-02 20:56:47 -0300

    feat: requirement 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 
+/*
+  A tabela deve possuir um cabeçalho com os seguintes valores:
+    - Descrição;
+    - Tag;
+    - Método de pagamento;
+    - Valor;
+    - Moeda;
+    - Câmbio utilizado;
+    - Valor convertido;
+    - Moeda de conversão;
+    - Editar/Excluir.
+*/
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Descrição</td>
+            <td>Tag</td>
+            <td>Método de pagamento</td>
+            <td>Valor</td>
+            <td>Moeda</td>
+            <td>Câmbio utilizado</td>
+            <td>Valor convertido</td>
+            <td>Moeda de conversão</td>
+            <td>Editar/Excluir</td>
+          </tr>
+        </tbody>
+      </table>
+
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
@@ -7,6 +8,7 @@ class Wallet extends React.Component {
     return (
       <section>
         <Header />
+        <Table />
         <WalletForm />
       </section>
     );
